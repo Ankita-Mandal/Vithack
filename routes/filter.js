bdc1b439f2bf66b1f7a01c493f615ada0f4031b8
Ankita-Mandal <anki.mandal@gmail.com>
2020-10-11 01:39:34 +0530

    first commit

--- routes/filter.js
+++ routes/filter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router =express.Router();
 // const Filter = require('../models/Filter')
-const jsonData = require('../mydata.json');
+const jsonData = require('../mydata2.json');
 
 
 
